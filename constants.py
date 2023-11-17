a0b514882007c149e7a828cda2bc2129b9b780af
Олег Мусатов <olegmusatov97@gmail.com>
2023-11-17 14:34:58 +0700

    Add prices and city

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 MAIN_URL = 'https://online.metro-cc.ru/'
+CATEGORY_EXAMPLE = 'category/molochnye-prodkuty-syry-i-yayca/syry/plavlenye'
 
 HEADERS = {
     'authority': 'online.metro-cc.ru',
@@ -19,7 +20,7 @@ HEADERS = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
 }
 
-COOKIES = {
+COOKIES_FOR_MOSCOW = {
     '_slid_server': '6555c114515748bbf2024e28',
     'exp_auth': '3Rd5fv0vTUylyTasdzAcqA.1',
     'is18Confirmed': 'false',
@@ -47,6 +48,42 @@ COOKIES = {
     'directCrm-session': '%7B%22deviceGuid%22%3A%22cde0e97a-3cf7-436a-a284-ba1a08d0918a%22%7D',
     'tmr_detect': '0%7C1700119143203',
 }
+
+COOKIES_FOR_SAINT_PETERSBURG = {
+    '_slid_server': '6555c114515748bbf2024e28',
+    '_slid_server': '6555c114515748bbf2024e28',
+    'exp_auth': '3Rd5fv0vTUylyTasdzAcqA.1',
+    'is18Confirmed': 'false',
+    '_slid': '6555c114515748bbf2024e28',
+    'metro_user_id': '3d4e4f37f16802a8a1794cce5d9a72ee',
+    '_ga': 'GA1.1.1202858113.1700118805',
+    'uxs_uid': 'a25730d0-844f-11ee-a782-8fe7d6f321e9',
+    '_ym_uid': '1700118806886464201',
+    '_ym_d': '1700118806',
+    '_ym_isad': '2',
+    '_gcl_au': '1.1.1248662484.1700118806',
+    'tmr_lvid': 'ea0b1709a077d32f473173fe5e4a8e1a',
+    'tmr_lvidTS': '1700118806631',
+    'flocktory-uuid': 'f8bc22ed-37a7-43e9-9684-8fb4ba9f2848-8',
+    'popmechanic_sbjs_migrations': 'popmechanic_1418474375998%3D1%7C%7C%7C1471519752600%3D1%7C%7C%7C1471519752605%3D1',
+    'sl_anh': '1',
+    '_slfreq': '633ff97b9a3f3b9e90027740%3A633ffa4c90db8d5cf00d7810%3A1700157762%3B64a81e68255733f276099da5%3A64abaf645c1afe216b0a0d38%3A1700157762%3B6490039afadf0f1d430966df%3A64e35a6b6c71a1d5090937fb%3A1700126005',
+    'mp_6d4d5c2aa2170f906c84f873ab89181b_mixpanel': '%7B%22distinct_id%22%3A%20%22%24device%3A18bd6fa3c652dab-0df411911412bb-16525634-13c680-18bd6fa3c652dab%22%2C%22%24device_id%22%3A%20%2218bd6fa3c652dab-0df411911412bb-16525634-13c680-18bd6fa3c652dab%22%2C%22%24search_engine%22%3A%20%22google%22%2C%22%24initial_referrer%22%3A%20%22https%3A%2F%2Fwww.google.com%2F%22%2C%22%24initial_referring_domain%22%3A%20%22www.google.com%22%7D',
+    'tmr_detect': '0%7C1700150728083',
+    'mindboxDeviceUUID': 'cde0e97a-3cf7-436a-a284-ba1a08d0918a',
+    'directCrm-session': '%7B%22deviceGuid%22%3A%22cde0e97a-3cf7-436a-a284-ba1a08d0918a%22%7D',
+    'pickupStore': '15',
+    'metroStoreId': '15',
+    '_ga_VHKD93V3FV': 'GS1.1.1700182195.5.1.1700182224.0.0.0',
+}
+
+
+
 PARAMS = {
     'from': 'under_search',
 }
+
+WRONG_CATEGORY = (
+    'Кажется, вы указали неправильную категорию. Пожалуйста,'
+    ' запустите скрипт снова и введите корректное название категории.'
+)
--- parse_script.py
+++ parse_script.py
@@ -2,156 +2,129 @@ import requests
 from bs4 import BeautifulSoup as bt
 import csv
 
-from constants import MAIN_URL, HEADERS, COOKIES, PARAMS
+from constants import (
+    MAIN_URL, HEADERS, COOKIES_FOR_MOSCOW, PARAMS, CATEGORY_EXAMPLE,
+    WRONG_CATEGORY, COOKIES_FOR_SAINT_PETERSBURG
+)
 
 
-def parse_auchan_category(category_url):
-
-    last_pages = 24
+def parse_metro_category(category_url):
+    start_url = f'{category_url}&page=1'
     all_products = []
-    for page in range(6, last_pages + 1):
-        print(page)
-        response = requests.get(f'{category_url}&page={page}', headers=HEADERS, cookies=COOKIES, params=PARAMS)
-        soup = bt(response.text, 'lxml')
-        products = soup.find_all(class_=('catalog-2-level-product-card product-card subcategory-or-type__products-item with-rating with-prices-drop has-online-range-prices', 'catalog-2-level-product-card product-card subcategory-or-type__products-item with-rating with-prices-drop', 'catalog-2-level-product-card product-card subcategory-or-type__products-item with-rating has-cashback with-prices-drop has-online-range-prices'))
-        for item in products:
-            link_element = item.find('a', class_='product-card-photo__link')
-            product_link = link_element.get('href')[1:]
-            print(f'{MAIN_URL}{product_link[1:]}')
+    print(f'Начинаю парсить товары с ресурса:\n{start_url}')
+    for cookies_for_city in [COOKIES_FOR_MOSCOW, COOKIES_FOR_SAINT_PETERSBURG]:
+        if cookies_for_city == COOKIES_FOR_MOSCOW:
+            print('начинаю парсить товары из города Москва')
+        else:
+            print('начинаю парсить товары из города Санкт-Петербург')
+        start_response = requests.get(
+            start_url, headers=HEADERS,
+            cookies=cookies_for_city, params=PARAMS
+        )
 
-            price_span = soup.find('span', class_='product-price__sum-rubles')
-            price = price_span.get_text(strip=True).replace(' ', '')
-            print(price)
-            # product_id = item.get('data-sku')
-            # print(product_id)
-            response = requests.get(f'{MAIN_URL}{product_link}', headers=HEADERS, cookies=COOKIES, params=PARAMS)
+        soup = bt(start_response.text, 'lxml')
+        list_items = soup.find_all('li', {'data-v-2505d9ee': True})
+        try:
+            last_pages = list_items[-2].get_text(strip=True)
+        except IndexError:
+            print(WRONG_CATEGORY)
+            return WRONG_CATEGORY
+
+        for page in range(1, int(last_pages) + 1):
+            print(page)
+            url = f'{category_url}&page={page}'
+            if url != start_url:
+                response = requests.get(
+                    f'{category_url}&page={page}', headers=HEADERS,
+                    cookies=COOKIES_FOR_SAINT_PETERSBURG, params=PARAMS
+                )
+            else:
+                response = start_response
+
             soup = bt(response.text, 'lxml')
-            # short_info = soup.find_all(class_='product-attributes__list style--product-page-short-list')
-            # print
-            id_element = soup.find('p', class_='product-page-content__article')
-            id = id_element.get_text(strip=True).split(' ')[-1]
+            products = soup.find_all(class_=('catalog-2-level-product-card product-card subcategory-or-type__products-item with-rating with-prices-drop has-online-range-prices', 'catalog-2-level-product-card product-card subcategory-or-type__products-item with-rating with-prices-drop', 'catalog-2-level-product-card product-card subcategory-or-type__products-item with-rating has-cashback with-prices-drop has-online-range-prices'))
+            print(len(products))
+            for place, item in enumerate(products):
+                print(place + 1)
+                link_element = item.find('a', class_='product-card-photo__link')
+                product_link = link_element.get('href')[1:]
 
-            name_element = soup.find('meta', {'itemprop': 'name'})
-            product_name = name_element.get('content')
+                is_promotion = item.find('span', class_='product-price nowrap product-unit-prices__actual style--catalog-2-level-product-card-major-actual color--red')
 
-            brand_element = soup.find('a', class_='product-attributes__list-item-link reset-link active-blue-text')
-            brand_name = brand_element.get_text(strip=True)
+                rubles = item.find('span', class_='product-price__sum-rubles').get_text(strip=True)
+                penny = item.find('span', class_='product-price__sum-penny')
+                if penny:
+                    penny = penny.get_text(strip=True)
+                    product_price = f'{rubles}{penny}'
+                    # print(penny)
+                else:
+                    product_price = rubles
 
+                if is_promotion:
+                    discount_price = product_price
+                    product_price = item.find('div', class_='product-unit-prices__old-wrapper')
+                    if product_price:
+                        old_price_in_rubles = product_price.find('span', class_='product-price__sum-rubles')
+                        old_price_in_penny = product_price.find('span', class_='product-price__sum-penny')
+                        if old_price_in_rubles and old_price_in_penny:
+                            old_price_in_rubles = old_price_in_rubles.get_text(strip=True)
+                            old_price_in_penny = old_price_in_penny.get_text(strip=True)
+                            product_price = f'{old_price_in_rubles}{old_price_in_penny}'
+                        elif old_price_in_rubles:
+                            product_price = old_price_in_rubles.get_text(strip=True)
+                        else:
+                            product_price = old_price_in_rubles
+                else:
+                    discount_price = None
+                print(f'обычная цена: {product_price}')
+                print(f'цена по скидке: {discount_price}')
 
-            # brand = short_info.find('a', class_='product-attributes__list-item-link reset-link active-blue-text')
-            print(id)
-            print(product_name)
-            print(brand_name)
-            
-            break
-        break
-        print(len(all_products))
-    print(len(all_products))
-    # for item in all_products:
-    #     item_id = item['data-offer-id']
-    
-    #     # Получаем текст внутри тега 'p'
-    #     title = item.find('p').text.strip()
-        
-    #     # Получаем атрибут href из тега 'a'
-    #     link = item.find('a')['href']
-        
-    #     # Ищем цену и валюту внутри тега 'div' с классом 'css-xtv3eo'
-    #     price_element = item.find('div', class_='css-xtv3eo')
-    #     price = price_element.text.strip()
+                response = requests.get(
+                    f'{MAIN_URL}{product_link}', headers=HEADERS,
+                    cookies=COOKIES_FOR_MOSCOW, params=PARAMS
+                )
+                soup = bt(response.text, 'lxml')
 
-        
-    #     # Получаем атрибут brand из скрипта с типом "application/ld+json"
-    #     script_data = item.find('script', type='application/ld+json').text
-    #     brand_start = script_data.find('"brand"') + 8
-    #     brand_end = script_data.find('"', brand_start)
-    #     brand = script_data[brand_start:brand_end]
-        
-    #     # Выводим полученные данные
-    #     print(f"Item ID: {item_id}")
-    #     print(f"Title: {title}")
-    #     print(f"Link: {MAIN_URL}{link}")
-    #     print(f"Price: {price}")
-    #     print(f"Brand: {brand}")
-    #     print("\n")
-    #     break
+                id_element = soup.find('p', class_='product-page-content__article')
+                id = id_element.get_text(strip=True).split(' ')[-1]
 
-    products = []
-    return products
+                name_element = soup.find('meta', {'itemprop': 'name'})
+                product_name = name_element.get('content')
+
+                brand_element = soup.find('a', class_=('product-attributes__list-item-link reset-link active-blue-text'))
+                brand_name = brand_element.get_text(strip=True)
+
+                all_products.append(
+                    [id, product_name, product_link, product_price, discount_price, brand_name]
+                )
+
+    len(all_products)
+    return all_products
 
 
 def save_to_csv(data, filename):
     with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['id', 'name', 'link', 'regular_price', 'promo_price', 'brand']
+        fieldnames = ['id товара', 'наименование', 'ссылка на товар', 'регулярная цена', 'промо цена', 'бренд']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         writer.writeheader()
-        for row in data:
-            writer.writerow(row)
+        for values in data:
+            writer.writerow({'id товара': values[0], 'наименование': values[1], 'ссылка на товар': values[2], 'регулярная цена': values[3], 'промо цена': values[4], 'бренд': values[5]})
 
 
 if __name__ == "__main__":
-    category_url = f"{MAIN_URL}category/molochnye-prodkuty-syry-i-yayca/syry?from=under_search"  # Замените на фактический URL категории товаров
-    products = parse_auchan_category(category_url)
+    category = input('''Введите правильное имя категории для парсера пример:\n
+category/molochnye-prodkuty-syry-i-yayca/syry/plavlenye \n
+Вы можете скопировать его из адресной строки на странице со списками
+товаров, можете нажать 1 и будет применена категория из примера:
+''')
+    if category == '1':
+        category = CATEGORY_EXAMPLE
+    category_url = f"{MAIN_URL}{category}?from=under_search"
+    products = parse_metro_category(category_url)
 
-    if len(products) > 100:
+    if products is not WRONG_CATEGORY and len(products) > 100:
         print("Более 100 товаров в категории. Сохранение данных.")
         save_to_csv(products, 'metro_products.csv')
     else:
-        print("Менее 100 товаров в категории.")
-
-
-# import requests
-# from bs4 import BeautifulSoup
-# import csv
-# import json
-
-# def parse_auchan_cheeses(category_url, headers, cookies):
-#     response = requests.get(category_url, headers=headers, cookies=cookies)
-#     soup = BeautifulSoup(response.content, 'html.parser')
-
-#     cheeses = []
-
-#     for cheese_card in soup.find_all('div', class_='cheese-card'):
-#         # Извлекаем необходимые данные
-#         cheese_id = cheese_card.find('script', type='application/ld+json').text.strip()
-#         cheese_id = json.loads(cheese_id).get('offers', {}).get('sku')
-        
-#         name = cheese_card.find('p', class_='css-1bdovxp').text.strip()
-#         link = cheese_card.find('a', class_='linkToPDP').get('href')
-#         regular_price = cheese_card.find('div', class_='css-xtv3eo').text.strip()
-#         promo_price = cheese_card.find('div', class_='css-1hxq85i').text.strip() if cheese_card.find('div', class_='css-1hxq85i') else None
-#         brand = json.loads(cheese_card.find('script', type='application/ld+json').text.strip()).get('brand')
-
-#         # Проверяем наличие сыра
-#         availability = cheese_card.find('span', class_='cheese-card__availability').text.strip()
-#         if availability.lower() == 'в наличии':
-#             cheeses.append({
-#                 'id': cheese_id,
-#                 'name': name,
-#                 'link': link,
-#                 'regular_price': regular_price,
-#                 'promo_price': promo_price,
-#                 'brand': brand
-#             })
-
-#     return cheeses
-
-# def save_to_csv(data, filename):
-#     with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
-#         fieldnames = ['id', 'name', 'link', 'regular_price', 'promo_price', 'brand']
-#         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-#         writer.writeheader()
-#         for row in data:
-#             writer.writerow(row)
-
-# if __name__ == "__main__":
-#     category_url = "https://www.auchan.ru/catalog/syry/tverdye-i-polutverdye/"  # Замените на фактический URL категории с сырами
-#     cheeses = parse_auchan_cheeses(category_url, headers, cookies)
-
-#     if len(cheeses) > 0:
-#         print("Сохранение данных о сырах.")
-#         save_to_csv(cheeses, 'auchan_cheeses.csv')
-#     else:
-#         print("Нет данных о сырах.")
+        print("Сохранение отменено.")
